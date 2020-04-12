9bbffffd1a16bd1c10e261279190e59789b8458a
Corri Golden <corri.golden@gmail.com>
2020-04-12 14:22:14 -0500

    finished

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+console.log("yo")
+
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+const myPet = {
+    name: "Dori",
+    species: "cat",
+    Nicknames: ["ugh", "What now", "Dorita"],
+    age: 3,
+
+}
+
+console.log(myPet, "myPet")
